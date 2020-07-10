1a0ff554999787406622d332c837b0ef14b1f1ca
Francisco <francisco.vera@rindegastos.com>
2020-07-10 11:48:31 -0400

    seccion de usuarios y autorizaciones auth

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -3,6 +3,7 @@ import { EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+import { User } from '../auth/user.entity';
 
 
 
@@ -10,9 +11,11 @@ import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 export class TaskRepository extends Repository<Task> {
 
 
-    async getTasks(filterDto: GetTaskFilterDto): Promise<Task[]> {
+    async getTasks(filterDto: GetTaskFilterDto, user: User): Promise<Task[]> {
         const { status, search } = filterDto;
         const query = this.createQueryBuilder('task');
+    
+        query.where('task.userId = :userId', { userId: user.id });
 
         if (status) {
             query.andWhere('task.status = :status', {status});
@@ -29,7 +32,7 @@ export class TaskRepository extends Repository<Task> {
     }
 
 
-    async createTask( createTaskDto:CreateTaskDto): Promise<Task> {
+    async createTask( createTaskDto:CreateTaskDto, user: User): Promise<Task> {
 
         const { title, description } = createTaskDto;
 
@@ -37,8 +40,11 @@ export class TaskRepository extends Repository<Task> {
         task.title = title;
         task.description = description;
         task.status = TaskStatus.OPEN;
+        task.user = user;
 
+        
         await task.save();
+        delete task.user;
         return task;
     }
 
